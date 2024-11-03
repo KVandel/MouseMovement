cf7f7cbdc4e26dab9ac91bf68794a3a5feb4b707
Kyaw Wunna Maung <kyawwunnamg.kwnm@gmail.com>
2024-11-03 09:36:56 +0630

    changed display settings. Now scroll bar is turned off.

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
 const root = document.querySelector("html");
 
 const cursor = document.createElement("div");
-cursor.innerHTML = "Kay";
+//cursor.innerHTML = "Kay";
 cursor.classList.add("cursor");
 root.appendChild(cursor);
 
 const cursor_follower = document.createElement("div");
-cursor_follower.innerHTML = "Kyaw";
+//cursor_follower.innerHTML = "Kyaw";
 cursor_follower.classList.add("cursor", "cursor_follower");
 root.appendChild(cursor_follower);
 
 root.addEventListener("mousemove", (e) => {
-  setPosition(cursor, e);
-  setPosition(cursor_follower, e);
+  setPosition(cursor, e, 0, 0);
+  setPosition(cursor_follower, e, 30, 20);
 });
 
-function setPosition(element, e) {
-  element.style.transform = `translate3d(${e.clientX}px, ${e.clientY}px,0)`;
+function setPosition(element, e, offx, offy) {
+  element.style.transform = `translate3d(${e.clientX + offx}px, ${
+    e.clientY + offy
+  }px,0)`;
 }
+/*
+root.addEventListener("touchmove", (e) => {
+  const touch = e.touches[0];
+  setPosition(cursor, touch.clientX, touch.clientY);
+  setPosition(cursor_follower, touch.clientX, touch.clientY);
+});
+
+function setPosition(element, x, y) {
+  element.style.transform = `translate3d(${touch.clientX}px, ${touch.clientY}px,0)`;
+}
+*/
