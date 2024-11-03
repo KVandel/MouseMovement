143260c562ca2899667968a230a062a83d3eda26
Kyaw Wunna Maung <kyawwunnamg.kwnm@gmail.com>
2024-11-03 10:06:38 +0630

    touch test commit

--- index.js
+++ index.js
@@ -11,23 +11,22 @@ cursor_follower.classList.add("cursor", "cursor_follower");
 root.appendChild(cursor_follower);
 
 root.addEventListener("mousemove", (e) => {
-  setPosition(cursor, e, 0, 0);
-  setPosition(cursor_follower, e, 30, 20);
+  setPosition(cursor, e.clientX, e.clientY, 0, 0);
+  setPosition(cursor_follower, e.clientX, e.clientY, 30, 20);
 });
-
+/*
 function setPosition(element, e, offx, offy) {
   element.style.transform = `translate3d(${e.clientX + offx}px, ${
     e.clientY + offy
   }px,0)`;
-}
-/*
+}*/
+
 root.addEventListener("touchmove", (e) => {
   const touch = e.touches[0];
-  setPosition(cursor, touch.clientX, touch.clientY);
-  setPosition(cursor_follower, touch.clientX, touch.clientY);
+  setPosition(cursor, touch.clientX, touch.clientY, 0, 0);
+  setPosition(cursor_follower, touch.clientX, touch.clientY, 30, 20);
 });
 
-function setPosition(element, x, y) {
-  element.style.transform = `translate3d(${touch.clientX}px, ${touch.clientY}px,0)`;
+function setPosition(element, x, y, offX, offY) {
+  element.style.transform = `translate3d(${x + offX}px, ${y + offY}px,0)`;
 }
-*/
